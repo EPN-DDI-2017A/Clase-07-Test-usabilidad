9a1b852d69d649773525ef13df6b79b0f7ccbc59
Chalo Salvador <chalosalvador@gmail.com>
2017-06-30 18:00:31 -0500

    clase 9 seleccion de checkboex

--- clase7/clase7/js/main.js
+++ clase7/clase7/js/main.js
@@ -11,25 +11,36 @@ $(document).ready(function () {
         var nombre = $('#nombre').val();
         var apellido = $('#apellido').val();
 
+        var numItems = $('#accordion .card').length;
+
+        var nuevaFila = '<div class="card">';
+        nuevaFila += '<div class="card-header" role="tab" id="heading'+ numItems+1 +'">';
+        nuevaFila += '<h5 class="mb-0">';
+        nuevaFila += '<a data-toggle="collapse" data-parent="#accordion" href="#collapse'+ numItems+1 +'" aria-expanded="false" aria-controls="collapse'+ numItems+1 +'">';
+        nuevaFila += nombre + ' ' + apellido;
+        nuevaFila += '</a>';
+        nuevaFila += '</h5>';
+        nuevaFila += '</div>';
+        nuevaFila += '<div id="collapse'+ numItems+1 +'" class="collapse" role="tabpanel" aria-labelledby="heading'+ numItems+1 +'">';
+        nuevaFila += '<div class="card-block">';
+        nuevaFila += '<ul class="list-group">';
+
         $.each($('#preferencias input[type="checkbox"]'),
             function (index, elemento) {
                 if ($(elemento).is(':checked')) {
-                    alert($(elemento).val());
+                    nuevaFila += '<li class="list-group-item">' + $(elemento).val() + '</li>';
                 }
-        });
+            });
+
+        nuevaFila += '</ul>';
+        nuevaFila += '</div>';
+        nuevaFila += '</div>';
+        nuevaFila += '</div>';
 
 
+        $('#accordion').append(nuevaFila);
 
-
-        var nuevaFila =
-            '<li class="list-group-item">'
-            + nombre + ' ' + apellido
-            + '</li>';
-
-
-        // $('#lista-usuarios').append(nuevaFila);
-
-        // $('#myModal').modal('hide');
+        $('#myModal').modal('hide');
     })
 
 });
